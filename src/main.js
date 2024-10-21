f91304ee2f89ba06b11b878f05e8e37cdd08cc39
Prem <prematid7@gmail.com>
2024-10-21 19:39:55 +0530

    Update page title in index.html

--- src/main.js
+++ src/main.js
@@ -1,10 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Get the root element where React will render the app
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+// Render the App component within a StrictMode wrapper
+root.render(
+  React.createElement(
+    React.StrictMode,
+    null,
+    React.createElement(App, null)
+  )
+);
+
+
+
+
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
