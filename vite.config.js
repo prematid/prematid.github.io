22324ff64fd88d24d030a3ab6aa83a0f6bd8759d
Prem <prematid7@gmail.com>
2024-10-21 11:08:44 +0530

    Update build outDir in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   base: '/prematid.github.io/',
   plugins: [react()],
+  build: {
+    outDir: 'dist', // This should point to the 'dist' folder
+},
 })
