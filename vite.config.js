dc2427a62c6e27ad90b834d4f68910e5c2e476c3
Prem <prematid7@gmail.com>
2024-10-21 20:19:37 +0530

    Update script source file extension in index.html

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/prematid.github.io/',
+  base: '/',
   plugins: [react()],
   build: {
-    outDir: 'dist', // This should point to the 'dist' folder
+    outDir: 'dist',
 },
 })
